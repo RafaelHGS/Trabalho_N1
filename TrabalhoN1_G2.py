c070d0e271b1d8c7010b47d55fb2ed9e22ad7113
RafaelHGS <89417905+RafaelHGS@users.noreply.github.com>
2021-09-08 09:27:13 -0300

    Organização
     De acordo com a atividade N1, não se pode ter comentários, mas ter um código legível.
     Portanto foi deixado apenas os nomes dos responsáveis pelo código, garantindo a leitura adequada do código

--- TrabalhoN1_G2.py
+++ TrabalhoN1_G2.py
@@ -9,39 +9,34 @@ Nome:   Gustavo Mello Soares dos Santos     RA: 21568663
 Nome:   Gabriel de Souza Alves              RA: 21587787
 '''
 
-#   Importação das bibliotecas
 import random
 
-#Declaração de variáveis: Antes do Início do programa é necessário declarar algumas variáveis essenciais
+caminho = 0     
+sala = 1        
+interacao = 0   
 
-caminho = 0     #É a variável responsável por guardar o caminho escolhido, inicializada em 0. Os caminhos vermelho e preto serão selecionados respectivamente por "1" e "2"
-sala = 1        #É a variável responsável por guardar a sala que a Guilda (Usuário) está no momento. Inicializada na sala de valor "1"
-interacao = 0   #Variavel relativa à quantidade de interações, iniciada em "0". Ela será responsável por guardar a quantidade total de movimentos/interações para "vencer" esse labirinto
 
-#Inicio do Programa
-while (1):  #Laço de repetição do inicio do programa
-    if (sala == 9) and (interacao <7):  #Verificação do número de salas, se for igual a 9 com menos de 7 interações, o jogador vence
+while (1):
+    if (sala == 9) and (interacao <7):
         print("\nParabéns você chegou ao fim do labirinto com {} interações, Vc e sua Guilda Venceram o Labirinto !!!".format(interacao))
         break
-    elif (sala == 9) and (interacao >= 7):  #Verificação do número de salas, se for igual a 9 com 7 ou mais interações, o jogador perde
+    elif (sala == 9) and (interacao >= 7):
         print("\n\tInfelizmente Você se Perdeu no meio do caminho e levou\n mais do que 7 interações para chegar na sala 9")
         print ("Tente novamente com menos movimentos/interações, e Boa Sorte!")
         print ("Total de Interações/Movimentos: {}".format(interacao))
         break
-    else:   #Execução do programa, onde o usuário vai selecionar o caminho que vai percorrer
+    else:
         print ("Você está na sala: {}".format(sala))    
         print ("Escolha seu caminho: ")
         print ("[1] - Caminho vermelho")
         print ("[2] - Caminho preto")
         caminho = int(input())
 
-        while (caminho <= 0) or (caminho >= 3):     #Verifica se está numa sala válida
+        while (caminho <= 0) or (caminho >= 3):
             print("\nSem trapacear !\n Você Deve escolher um caminho 'vermelho' ou 'preto' para prosseguir !\n")
             print ("Você está na sala: {}".format(sala))    
             print ("Escolha seu caminho: ")
             print ("[1] - Caminho vermelho")
             print ("[2] - Caminho preto")
             caminho = int(input())
-else:
-    print("Você se Perdeu no labirinto para todo o sempre buahahahahahahahaha !") # Easter-egg no Código
   
